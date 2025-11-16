784503a0e3f8d68fc1cb2c74283c891043fac6ba
Necmettin <necmettin.solmaz@icisleri.gov.tr>
2025-11-16 22:57:00 +0300

    dosya yolu için düzenleme yapıldı.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  // 1. Düzeltme: Base yolu (path) depo adı olarak ayarla
+  base: `/${repoName}/`, 
+  // base: '/odev-takip-react/', // Eğer depo adını biliyorsanız doğrudan yazabilirsiniz
            
 })

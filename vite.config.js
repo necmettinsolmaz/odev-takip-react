c40b90e005947fff2a8871c30709568c13e4b521
Necmettin <necmettin.solmaz@icisleri.gov.tr>
2025-11-17 21:03:13 +0300

    Son Vite base path fix ve yeni build

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  // 1. Düzeltme: Base yolu (path) depo adı olarak ayarla
   base: '/odev-takip-react/',
-  // base: '/odev-takip-react/', // Eğer depo adını biliyorsanız doğrudan yazabilirsiniz
            
 })
